5ae9c5c268f0c5b35e76baf7e6edfe52923617ba
= mlm-games <= gerrit.r6a6a@passinbox.com>
2024-09-24 22:23:03 +0530

    update garden official orangefox's vendorsetup to 12.1 (add to credits)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,7 +19,7 @@
 #
 
 #set -o xtrace
-FDEVICE="garnet"
+FDEVICE="dandelion"
 THIS_DEVICE=${BASH_ARGV[2]}
 
 fox_get_target_device() {
@@ -42,21 +42,58 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 		exit 1
 	fi
 
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v27.0.zip
-	export FOX_VIRTUAL_AB_DEVICE=1
-    export FOX_VANILLA_BUILD=1
-    export FOX_ENABLE_APP_MANAGER=1
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_LZ4_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export FOX_USE_ZSTD_BINARY=1
-	export FOX_USE_NANO_EDITOR=1
-    	export FOX_DELETE_AROMAFM=1
+export TW_DEFAULT_LANGUAGE="en"
+export LC_ALL="C"
+export ALLOW_MISSING_DEPENDENCIES=true
+
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/bootdevice/by-name/recovery"
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
+
+# Alternate Codenames
+export TARGET_DEVICE_ALT="dandelion,angelica,angelican,angelicain,cattail"
+
+# Removed: OF_USE_GREEN_LED (deprecated)
+export OF_HIDE_NOTCH=1
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_NO_MIUI_PATCH_WARNING=1
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export OF_QUICK_BACKUP_LIST="/boot;/data;"
+export OF_PATCH_AVB20=1
+# Removed: FOX_DELETE_AROMAFM (no longer needed in 12.1)
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"
+export FOX_ENABLE_APP_MANAGER=1
+
+# R12.1 Settings
+export FOX_VERSION="R12.1_1"
+export FOX_BUILD_TYPE="Beta"
+export OF_MAINTAINER="Test"
+
+# OTA
+export OF_KEEP_DM_VERITY=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+
+# Screen Settings
+export OF_SCREEN_H=2400
+export OF_STATUS_H=80
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_CLOCK_POS=1
+export OF_ALLOW_DISABLE_NAVBAR=0
+
+# New flags for 12.1
+export OF_ENABLE_LPTOOLS=1
 else
 	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
 		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
